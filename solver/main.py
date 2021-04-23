8a17bf85c77dd09794415b36b431d1f920f31435
pymole <goglbummm@gmail.com>
2021-04-23 12:58:14 +0500

    Actions

--- solver/main.py
+++ solver/main.py
@@ -3,13 +3,18 @@ from actions import Action
 import random
 
 
+class Solver:
+    def __init__(self):
+        pass
+
+
 def main():
     while True:
         state = input()
         
         time.sleep(0.9)
         
-        print(random.choice(list(Action)))
+        print(random.choice(list(Action)).value)
 
 
 main()
